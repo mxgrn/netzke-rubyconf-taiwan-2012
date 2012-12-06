a4077623185276cf97dcff4b46605bddc02ede81
nomadcoder <nmcoder@gmail.com>
2012-12-06 13:08:44 +0800

    Add Ext JS and icons

---
+++ public/extjs/examples/writer/remote/app/controllers/example.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @class Users
+ * A simple application controller extension
+ */
+class Example extends ApplicationController {
+
+    public function reset() {
+        $res = new Response();
+        $res->success = true;
+
+        global $dbh;
+        $dbh->reset();
+
+        return $res->to_json();
+    }
+
+}
