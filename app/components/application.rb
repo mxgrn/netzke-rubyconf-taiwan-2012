85bd0c820eaf293fbbcb2ea1ab19c9d0cde1e977
nomadcoder <nmcoder@gmail.com>
2012-12-06 17:40:39 +0800

    Minor adjustments

--- app/components/application.rb
+++ app/components/application.rb
@@ -6,18 +6,21 @@ class Application < Netzke::Basepack::Viewport
   end
 
   component :books do |c|
+    c.force_fit = true
     c.region = :north
     c.split = true
     c.height = 200
   end
 
   component :authors do |c|
+    c.force_fit = true
     c.region = :west
     c.split = true
     c.width = 400
   end
 
   component :friends do |c|
+    c.force_fit = true
     c.region = :center
   end
 end
--- app/components/books.rb
+++ app/components/books.rb
@@ -1,6 +1,14 @@
 class Books < Netzke::Basepack::Grid
   include ColumnActions
 
+  column :created_at do |c|
+    c.hidden = true
+  end
+
+  column :updated_at do |c|
+    c.hidden = true
+  end
+
   def configure(c)
     super
     c.model = "Book"
