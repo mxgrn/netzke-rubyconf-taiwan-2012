63cd4ccabb571e9355f51ffec00ae08f0bf775e5
nomadcoder <nmcoder@gmail.com>
2012-12-06 13:06:53 +0800

    rails new netzke-rubyconf-taiwan-2012

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetzkeRubyconfTaiwan2012::Application.config.secret_token = '76ecc1345bc9169ddae1d869a1e597cba408d6b0715bea55e89f60404b34f477fbca0ddb982bec47e6e703522bedf5e9e2efbb14677512933ebe83798f9de7a1'
