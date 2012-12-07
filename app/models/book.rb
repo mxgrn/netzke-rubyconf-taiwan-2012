91f2ab7f138ebf5385e72dff2c1af50e143dd6b7
nomadcoder <nmcoder@gmail.com>
2012-12-07 09:12:49 +0800

    Extend attr_accessbile list for Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  attr_accessible :digital, :purchased_on, :title
+  attr_accessible :digital, :purchased_on, :title, :author_id, :borrowed_to_id
   belongs_to :author
   belongs_to :borrowed_to, class_name: "Friend"
 end
